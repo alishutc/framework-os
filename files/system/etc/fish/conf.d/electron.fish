05b7607efb617e8fa4c73d76c0593ef5072d1947
Alistair Hutchison <github@alistairh.co.uk>
2026-09-09 22:30:09 +0100

    Add native Visual Studio Code RPM and Wayland scaling support

---
+++ files/system/etc/fish/conf.d/electron.fish
@@ -0,0 +1,2 @@
+# Enable native Wayland for Electron apps
+set -gx ELECTRON_OZONE_PLATFORM_HINT auto
---
+++ files/system/etc/profile.d/electron.sh
@@ -0,0 +1,2 @@
+# Enable native Wayland for Electron apps
+export ELECTRON_OZONE_PLATFORM_HINT="auto"
